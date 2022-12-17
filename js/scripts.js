92625d53f8e02ad8cafa72ef2696c4353ffe2652
Rajdeep Hundal <hrajdeep712@gmail.com>
2022-12-16 19:06:20 -0600

    Working on automating scss update

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+ /* For activating tooltip
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+        */
+
+        $(function(){   
+
+            $(".carousel").carousel({interval: 2000});
+            $("#carouselButton").click(function(){
+                if($("#carouselButton").children("i").hasClass("fa-pause")) {
+
+                    $(".carousel").carousel("pause");
+                    $("#carouselButton").children("i").removeClass("fa-pause");
+                    $("#carouselButton").children("i").addClass("fa-play");
+                }else {
+
+                        $(".carousel").carousel("cycle");
+                        $("#carouselButton").children("i").removeClass("fa-play");
+                        $("#carouselButton").children("i").addClass("fa-pause");
+
+                }
+ 
+            });
+            
+        });
+
+        /* script for group buttons - pause and play
+        $(function(){   
+
+            $(".carousel").carousel({interval: 2000});
+            $("#carouselPause").click(function(){
+                $(".carousel").carousel("pause");
+            });
+            $("#carouselPlay").click(function(){
+                $(".carousel").carousel("cycle");
+            })
+        }); */
